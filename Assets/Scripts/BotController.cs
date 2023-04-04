2888d8019437b09eeccdfde8114d182b15733d33
Ngo Tien Dung <tiendung2000tn@gmail.com>
2023-04-04 10:11:22 +0700

    Revert "Backup"
    This reverts commit 8431979f83dee98df3f65ca1cbb927ba6eb7df3b.

--- Assets/Scripts/BotController.cs
+++ Assets/Scripts/BotController.cs
@@ -24,12 +24,12 @@ public class BotController : CharacterController
             ChangeState(new SeekBrickState());
 
 
-            Rand = UnityEngine.Random.Range(0, 10);
+            Rand = UnityEngine.Random.Range(0, 3);
             for (int j = 0; j < 100; j++)
             {
                 if (temp.Contains(Rand))
                 {
-                    Rand = UnityEngine.Random.Range(0, 10);
+                    Rand = UnityEngine.Random.Range(0, 3);
                 }
                 else break;
 
@@ -49,6 +49,7 @@ public class BotController : CharacterController
 
     private void Update()
     {
+        // goi ham xu ly
         if (currentState != null)
         {
             currentState.OnExecute(this);
--- Assets/Scripts/BrickGenerator.cs
+++ Assets/Scripts/BrickGenerator.cs
@@ -30,7 +30,7 @@ public class BrickGenerator : MonoBehaviour
     {
         OnInit();
 
-        InvokeRepeating(nameof(GenerateRemovedBrick), 1f, 3f);
+        InvokeRepeating(nameof(GenerateRemovedBrick), 1f, 1f);
     }
 
     void OnInit()
@@ -64,6 +64,7 @@ public class BrickGenerator : MonoBehaviour
 
             createBrick.transform.SetParent(this.transform);
             createBrick.GetComponent<Brick>().numberBrick = i;
+            //Debug.Log(createBrick.transform.position);
             InsertIntoArray(createBrick, createBrick.GetComponent<Brick>().brickColor, i);
         }
     }
